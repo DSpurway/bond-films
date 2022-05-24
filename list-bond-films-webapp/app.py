e44a3596a3662b3433c9920c6fcc2b8c5bccb9ef
DSpurway <53935437+DSpurway@users.noreply.github.com>
2022-05-24 10:40:29 +0100

    Update app.py

--- list-bond-films-webapp/app.py
+++ list-bond-films-webapp/app.py
@@ -6,15 +6,14 @@ import sys
 app = Flask(__name__)
 
 @app.route('/')
-def index():
+def list():
     url = "https://www.pocket-lint.com/tv/news/148096-james-bond-007-best-movie-viewing-order-chronological-release"
     html = urlopen(url)
     soup = BeautifulSoup(html, 'html')
     # soup.contents
     Bond_Film_Quick_List = soup.find("p", string="This is the same list as above, only spoiler-free and much quicker to read:")
     Bond_Film_Date_List=Bond_Film_Quick_List.next_sibling.text
-    
-response = Bond_Film_Date_List
+    return Bond_Film_Date_List
 
 @app.route('/healthz')
 # Added healthcheck endpoint

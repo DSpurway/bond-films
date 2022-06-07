669d555169f5f39f5a80c530ef1830abfd79aa12
DSpurway <53935437+DSpurway@users.noreply.github.com>
2022-06-07 16:29:54 +0200

    Update app.py

--- list-bond-girls-webapp/app.py
+++ list-bond-girls-webapp/app.py
@@ -1,31 +1,40 @@
-#!/usr/bin/env python
-# coding: utf-8
-
+from flask import Flask, request
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
 import fileinput
 
-url = "https://en.wikipedia.org/wiki/Bond_girl"
-html = urlopen(url)
-soup = BeautifulSoup(html, 'html')
-# soup.contents
-#print(soup)
-Femme_Fatale_href = soup.find('a',href="/wiki/Femme_fatale")
-Bond_Girls_Table=Femme_Fatale_href.find_next('table')
-Bond_Girls_Text_List=Bond_Girls_Table.text
-#print(Bond_Girls_Text_List)
+app = Flask(__name__)
 
-Actress_Count=Bond_Girls_Text_List.count("(")
-Char=0
-End=0
-Actresses_List = []
-
-for Actress in range(Actress_Count):
-    Start = Bond_Girls_Text_List.find("(", End)
-    Start += 1  # skip the bracket, move to the next character
-    End = Bond_Girls_Text_List.find(")", Start)
-    Result=Bond_Girls_Text_List[Start:End]
-    if Actresses_List.count(Result) == 0:
-        Actresses_List.append(Result)    
+@app.route('/')
+def list():
+    url = "https://en.wikipedia.org/wiki/Bond_girl"
+    html = urlopen(url)
+    soup = BeautifulSoup(html, 'html')
+    # soup.contents
+    Femme_Fatale_href = soup.find('a',href="/wiki/Femme_fatale")
+    Bond_Girls_Table=Femme_Fatale_href.find_next('table')
+    Bond_Girls_Text_List=Bond_Girls_Table.text
     
-print(Actresses_List)
+    Actress_Count=Bond_Girls_Text_List.count("(")
+    Char=0
+    End=0
+    Actresses_List = []
+    
+    for Actress in range(Actress_Count):
+        Start = Bond_Girls_Text_List.find("(", End)
+        Start += 1  # skip the bracket, move to the next character
+        End = Bond_Girls_Text_List.find(")", Start)
+        Result=Bond_Girls_Text_List[Start:End]
+        
+        if Actresses_List.count(Result) == 0:
+            Actresses_List.append(Result)    
+        
+    return Actresses_List
+
+@app.route('/healthz')
+# Added healthcheck endpoint
+def healthz():
+    return "ok"
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=8080)

ee43d22fbb9716a7b8b9d63f206cbf2a2d1d072c
DSpurway <53935437+DSpurway@users.noreply.github.com>
2023-06-30 17:11:24 +0100

    Changed find

--- list-bond-films-webapp/app.py
+++ list-bond-films-webapp/app.py
@@ -11,8 +11,10 @@ def list():
     html = urlopen(url)
     soup = BeautifulSoup(html, 'html')
     # soup.contents
+
     Bond_Film_Quick_List = soup.find("p", string="This is the same list as above, only spoiler-free and much quicker to read:")
-    Bond_Film_Date_List=Bond_Film_Quick_List.next_sibling.text
+    Bond_Film_Date_List=Bond_Film_Quick_List.find_next("ul").text
+# Bond_Film_Date_List=Bond_Film_Quick_List.next_sibling.text
     return Bond_Film_Date_List
 
 @app.route('/healthz')
